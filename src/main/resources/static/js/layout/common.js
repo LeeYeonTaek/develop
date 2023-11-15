273368c51c873e4912e62a476cfcef4db3babfae
wbol3298 <wbol3298@naver.com>
2023-11-16 00:09:29 +0900

    Add responsive

--- src/main/resources/static/js/layout/common.js
+++ src/main/resources/static/js/layout/common.js
@@ -5,3 +5,24 @@
             $(this).find('.submenu').stop().slideUp();
         });
     });
+    $(function() {
+        const menu = $("#responsive-menu");
+        const menuItems = menu.find("li");
+        function adjustMenu() {
+            const windowWidth = $(window).width();
+
+            if (windowWidth < 600) {
+                menuItems.css({
+                    "display": "block",
+                    "margin-right": "0"
+                });
+            } else {
+                menuItems.css({
+                    "display": "inline-block",
+                    "margin-right": "40px"
+                });
+            }
+        }
+        adjustMenu();
+        $(window).on("resize", adjustMenu);
+    });
--- src/main/webapp/WEB-INF/tiles/layouts/header.jsp
+++ src/main/webapp/WEB-INF/tiles/layouts/header.jsp
@@ -1,7 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <br/>
 <div class="menu-wrap">
-<ul class="menu">
+<ul class="menu" id="responsive-menu">
     <li><a href="#home">홈</a></li>
     <li><a href="#board">게시판</a></li>
     <li><a href="#services">서비스</a>
