cc465bf6751c49620de36dbac08db8cec0ccbdd0
wbol3298 <wbol3298@naver.com>
2023-12-21 23:03:29 +0900

    Interceptor application incomplete

---
+++ src/main/java/com/example/develop/interceptor/CustomInterceptor.java
@@ -0,0 +1,25 @@
+package com.example.develop.interceptor;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CustomInterceptor extends HandlerInterceptorAdapter {
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null && authentication.getPrincipal() instanceof UserDetails) {
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+            modelAndView = new ModelAndView();
+            modelAndView.addObject("currentUserDetails", userDetails);
+        }
+    }
+
+}
