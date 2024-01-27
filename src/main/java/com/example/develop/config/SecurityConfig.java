13f77c712f62c958cb806baae6d79cdf6b7f90d6
wbol3298 <wbol3298@naver.com>
2024-01-27 23:54:47 +0900

    spring boot version up, Security code change

--- src/main/java/com/example/develop/config/SecurityConfig.java
+++ src/main/java/com/example/develop/config/SecurityConfig.java
@@ -1,18 +1,15 @@
 package com.example.develop.config;
 
-import com.example.develop.authentication.CustomAuthenticationEntryPoint;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
@@ -23,23 +20,29 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http.csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/", "/main", "/member/join", "/member/login", "/css/**", "/js/**", "/scss/**", "/vendor/**", "/images/**", "/layouts/**", "/img/**").permitAll()
-                .antMatchers(HttpMethod.POST,"/","/member/**").permitAll()
-                .antMatchers("/member/**").hasRole("USER")
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/member/login")
-                .usernameParameter("memberName")
-                .passwordParameter("memberPw")
-                .loginProcessingUrl("/loginProc")
-                .defaultSuccessUrl("/")
-                .and()
-                .logout()
-                .logoutUrl("/member/logout")
-                .logoutSuccessUrl("/");
+        http
+                .csrf(csrfConfig -> csrfConfig.disable())
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/", "/main", "/member/join", "/member/login", "/css/**", "/js/**", "/scss/**", "/vendor/**", "/images/**", "/layouts/**", "/img/**").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/", "/member/**").permitAll()
+                                .requestMatchers("/member/**").hasRole("USER")
+                                .anyRequest().authenticated()
+                )
+                .formLogin(formLogin ->
+                        formLogin
+                                .loginPage("/member/login")
+                                .usernameParameter("memberName")
+                                .passwordParameter("memberPw")
+                                .loginProcessingUrl("/loginProc")
+                                .defaultSuccessUrl("/", true)
+                )
+                .logout(logout ->
+                        logout
+                                .logoutUrl("/member/logout")
+                                .logoutSuccessUrl("/")
+                );
+
         return http.build();
     }
 
